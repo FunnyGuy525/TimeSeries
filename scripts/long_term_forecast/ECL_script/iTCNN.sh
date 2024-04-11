b63b8188b2595d2caf62c9d49f9bdafc6c91f250
FunnyGuy525 <2377270644@qq.com>
2024-04-11 23:23:52 +0800

    add scripts

---
+++ scripts/long_term_forecast/ECL_script/iTCNN.sh
@@ -0,0 +1,99 @@
+export CUDA_VISIBLE_DEVICES=1
+
+model_name=iTCNN
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/electricity/ \
+  --data_path electricity.csv \
+  --model_id ECL_720_96 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 720 \
+  --label_len 48 \
+  --pred_len 96 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 321 \
+  --dec_in 321 \
+  --c_out 321 \
+  --d_model 128 \
+  --d_ff 128 \
+  --top_k 5 \
+  --des 'Exp' \
+  --itr 1
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/electricity/ \
+  --data_path electricity.csv \
+  --model_id ECL_720_192 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 720 \
+  --label_len 48 \
+  --pred_len 192 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 321 \
+  --dec_in 321 \
+  --c_out 321 \
+  --d_model 128 \
+  --d_ff 128 \
+  --top_k 5 \
+  --des 'Exp' \
+  --itr 1
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/electricity/ \
+  --data_path electricity.csv \
+  --model_id ECL_720_336 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 720 \
+  --label_len 48 \
+  --pred_len 336 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 321 \
+  --dec_in 321 \
+  --c_out 321 \
+  --d_model 128 \
+  --d_ff 128 \
+  --top_k 5 \
+  --des 'Exp' \
+  --itr 1
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/electricity/ \
+  --data_path electricity.csv \
+  --model_id ECL_720_720 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 720 \
+  --label_len 48 \
+  --pred_len 720 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 321 \
+  --dec_in 321 \
+  --c_out 321 \
+  --d_model 128 \
+  --d_ff 128 \
+  --top_k 5 \
+  --des 'Exp' \
+  --itr 1
---
+++ scripts/long_term_forecast/Exchange_script/iTCNN.sh
@@ -0,0 +1,101 @@
+export CUDA_VISIBLE_DEVICES=0
+
+model_name=iTCNN
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/exchange_rate/ \
+  --data_path exchange_rate.csv \
+  --model_id Exchange_48_96 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 48 \
+  --label_len 48 \
+  --pred_len 96 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 8 \
+  --dec_in 8 \
+  --c_out 8 \
+  --d_model 32 \
+  --d_ff 64 \
+  --top_k 5 \
+  --des 'Exp' \
+  --itr 1
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/exchange_rate/ \
+  --data_path exchange_rate.csv \
+  --model_id Exchange_48_192 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 48 \
+  --label_len 48 \
+  --pred_len 192 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 8 \
+  --dec_in 8 \
+  --c_out 8 \
+  --d_model 64 \
+  --d_ff 64 \
+  --top_k 5 \
+  --des 'Exp' \
+  --itr 1
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/exchange_rate/ \
+  --data_path exchange_rate.csv \
+  --model_id Exchange_48_336 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 48 \
+  --label_len 48 \
+  --pred_len 336 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 8 \
+  --dec_in 8 \
+  --c_out 8 \
+  --d_model 32 \
+  --d_ff 32 \
+  --top_k 5 \
+  --des 'Exp' \
+  --itr 1 \
+  --train_epochs 1
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/exchange_rate/ \
+  --data_path exchange_rate.csv \
+  --model_id Exchange_48_720 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 48 \
+  --label_len 48 \
+  --pred_len 720 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 8 \
+  --dec_in 8 \
+  --c_out 8 \
+  --d_model 32 \
+  --d_ff 32 \
+  --top_k 5 \
+  --des 'Exp' \
+  --itr 1 \
+  --train_epochs 1
